d1aa3b8c2c7fe4c2eef88fad5602dba3b51888cd
Robert <robert@cibb.t>
2025-12-15 22:49:13 +0100

    solve 2025/11/2

--- src/main/kotlin/aoc2025/11.kt
+++ src/main/kotlin/aoc2025/11.kt
@@ -6,6 +6,7 @@ import feedForward
 import reachability
 import readInput
 import solveA
+import solveB
 
 private fun main() {
     val connections = readInput(::main).trim().split("\n").map { line ->
@@ -15,13 +16,23 @@ private fun main() {
 
     val graph = Graph(connections)
     val start = graph.nodes.single { it.name == "you" }
-    val end = graph.nodes.single { it.name == "out" }
-    val paths = start.feedForward(start.reachability().reachableNodes, 1) { inputs ->
+    val out = graph.nodes.single { it.name == "out" }
+    val paths = findPaths(start, out, 1)
+    solveA(::main, paths)
+
+    val svr = graph.nodes.single { it.name == "svr" }
+    val dac = graph.nodes.single { it.name == "dac" }
+    val fft = graph.nodes.single { it.name == "fft" }
+    val p1 = findPaths(svr, fft, 1)
+    val p2 = findPaths(fft, dac, p1)
+    val p3 = findPaths(dac, out, p2)
+    solveB(::main, p3)
+}
+
+private fun findPaths(start: Graph.Node, end: Graph.Node, input: Long): Long {
+    return start.feedForward(start.reachability().reachableNodes, input) { inputs ->
         inputs.sum()
     }.getValue(end)
-
-    solveA(::main, paths)
-    //solveB(::main, resultB)
 }
 
 class Graph(val connections: Map<String, List<String>>) {
